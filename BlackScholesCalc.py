b891cf3caa268d07ebafa3c11c1d702426395ff1
aarnavyedla <yedla.aarnav@gmail.com>
2025-06-20 18:32:18 -0700

    Working option pricer given inputs

--- BlackScholesCalc.py
+++ BlackScholesCalc.py
@@ -16,26 +16,22 @@ def PutBlackCalc(s,x,r,t,v):
     return pprice
 
 def main():
-    '''stock = 40
-    strike = 50
+    '''
+    stock = 62
+    strike = 60
     interest = 0.04
     time = 40/365
     vol = 0.32
     print(CallBlackCalc(stock,strike,interest,time,vol),PutBlackCalc(stock,strike,interest,time,vol))'''
     st.title('Black Scholes Price Calculator')
-    st.text_input("Strike price", key="strike")
-    st.text_input("Current asset price", key="stock")
-    st.text_input("Risk free interest rate", key="interest")
-    st.text_input("Time till expiry", key="time")
-    st.text_input("Volatility", key="vol")
-    st.write('The call price is: ' + str(
-        CallBlackCalc(int(st.session_state.strike), int(st.session_state.stock), int(st.session_state.interest),
-                      int(st.session_state.time),
-                      int(st.session_state.vol))))
-    st.write('The put price is: ' + str(
-        PutBlackCalc(int(st.session_state.strike), int(st.session_state.stock), int(st.session_state.interest),
-                      int(st.session_state.time),
-                      int(st.session_state.vol))))
+    stock = st.number_input("Current asset price", key="stock")
+    strike = st.number_input("Strike price", key="strike")
+    interest = st.number_input("Risk free interest rate (%)", key="interest")/100
+    time = st.number_input("Days till expiry", key="time")/365
+    vol = st.number_input("Volatility", key="vol")
+    if strike != 0 and stock != 0 and interest != 0 and time != 0 and vol != 0:
+        st.write('The call price is: ' + str(CallBlackCalc(stock, strike, interest, time, vol)))
+        st.write('The put price is: ' + str(PutBlackCalc(stock, strike, interest, time, vol)))
 
 
 if __name__=="__main__":
